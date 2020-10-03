715d748312e78284fd6293c629f506ace2fffeab
KANISHKA GIMHAN <kanishkagimhan@gmail.com>
2020-10-03 18:53:03 +0530

    Initial commit

---
+++ D_Type/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(D_Type.Startup))]
+namespace D_Type
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
